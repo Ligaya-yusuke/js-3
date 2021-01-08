c3a2df7be9d30b49d50c59da68e55e0c0cbb5c3e
Ligaya-yusuke <e98069@gmail.com>
2021-01-08 13:37:24 +0900

    課題③送付

--- js-3.js
+++ js-3.js
@@ -3,7 +3,7 @@
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-
+    // ID欄作成
     const ul = document.getElementById("todo-list");
     const idDiv = document.getElementById("id-list");
     const idTitle = document.getElementById("id-title")
@@ -16,32 +16,31 @@ const onClickAdd = () => {
     });
     idList.textContent = index;
     console.log(index)
-    // };
     ul.appendChild(idDiv);
     idDiv.appendChild(idTitle);
     idTitle.appendChild(idList);
     
+    // コメント欄作成
     const commentDiv = document.getElementById("comment-list");
     const commentTitle = document.getElementById("comment-title");
     const commentList = document.createElement("li");
     commentList.className = "comment";
     commentList.textContent = inputText;
-    
     ul.appendChild(commentDiv);
     commentDiv.appendChild(commentTitle);
     commentTitle.appendChild(commentList);
   
+    // 状態欄作成
     const conditionDiv = document.getElementById("condition-list");
     const conditionTitle = document.getElementById("condition-title")
     const conditionButton = document.createElement("li");
     conditionButton.className = "conditionButton";
     const doingButton = document.createElement("button");
     doingButton.className = "doing";
-    doingButton.innerText = "ä½œæ¥­ä¸­"; 
+    doingButton.innerText = "作業中­"; 
     const deleteButton = document.createElement("button");
     deleteButton.className = "delete";
-    deleteButton.innerText = "å‰Šé™¤"; 
-
+    deleteButton.innerText = "削除"; 
     ul.appendChild(conditionDiv);
     conditionDiv.appendChild(conditionTitle);
     conditionTitle.appendChild(conditionButton);
